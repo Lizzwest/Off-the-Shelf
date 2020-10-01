20285eab15fecf9324dd327396a57882c1677cbe
Lizz <lizwesterband@gmail.com>
2020-10-01 14:39:13 -0700

    lock pipenv after adding requests and xmltodict

--- main_app/views.py
+++ main_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-import requests, xmltodict, json, dotenv
+import requests, xmltodict, json
 from decouple import config
 import os
 from django.core.paginator import Paginator
