b5dce1c695e548961855666327b124bed0de136c
Lizz <lizwesterband@gmail.com>
2020-10-02 11:33:59 -0700

    revert settings.py to allow us to push to heroku

--- offtheshelf/settings.py
+++ offtheshelf/settings.py
@@ -17,25 +17,25 @@ import socket
 import psycopg2
 import dj_database_url
 
-# DATABASE_URL = os.environ['DATABASE_URL']
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # If the host name starts with 'live', DJANGO_HOST = "production"
-# if socket.gethostname().startswith('live'):
-#     DJANGO_HOST = "production"
-# # Else if host name starts with 'test', set DJANGO_HOST = "test"
-# elif socket.gethostname().startswith('test'): 
-#     DJANGO_HOST = "testing"
-# else:
-# # If host doesn't match, assume it's a development server, set DJANGO_HOST = "development"
-#     DJANGO_HOST = "development"
-# # Define general behavior variables for DJANGO_HOST and all others
-# if DJANGO_HOST == "production":
-#     DEBUG = False
-#     STATIC_URL = 'https://offtheshelff.herokuapp.com/'
-# else:
-DEBUG = True
-STATIC_URL = '/static/'
+if socket.gethostname().startswith('live'):
+    DJANGO_HOST = "production"
+# Else if host name starts with 'test', set DJANGO_HOST = "test"
+elif socket.gethostname().startswith('test'): 
+    DJANGO_HOST = "testing"
+else:
+# If host doesn't match, assume it's a development server, set DJANGO_HOST = "development"
+    DJANGO_HOST = "development"
+# Define general behavior variables for DJANGO_HOST and all others
+if DJANGO_HOST == "production":
+    DEBUG = False
+    STATIC_URL = 'https://offtheshelff.herokuapp.com/'
+else:
+    DEBUG = True
+    STATIC_URL = '/static/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,9 +49,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY = os.environ['SECRET_KEY'] 
+SECRET_KEY = os.environ['SECRET_KEY'] 
 # or config('SECRET_KEY')
-SECRET_KEY  = "hello"
 
 # # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool)
@@ -152,30 +151,30 @@ USE_L10N = True
 
 USE_TZ = True
 
-# # Define EMAIL_BACKEND variable for DJANGO_HOST
-# if DJANGO_HOST == "production":
-#     # Output to SMTP server on DJANGO_HOST production
-#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# elif DJANGO_HOST == "testing":
-#     # Nullify output on DJANGO_HOST test
-#     EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
-# else: 
-#     # Output to console for all others
-#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# # Define CACHES variable for DJANGO_HOST production and all other hosts 
-# if DJANGO_HOST == "production":
-#    # Set cache
-#    CACHES = {
-#         'default': {
-#             'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-#             'LOCATION': '127.0.0.1:11211',
-#             'TIMEOUT':'1800',
-#             }
-#         }
-#    CACHE_MIDDLEWARE_SECONDS = 1800
-# else: 
-#    # No cache for all other hosts
-#    pass
+# Define EMAIL_BACKEND variable for DJANGO_HOST
+if DJANGO_HOST == "production":
+    # Output to SMTP server on DJANGO_HOST production
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+elif DJANGO_HOST == "testing":
+    # Nullify output on DJANGO_HOST test
+    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+else: 
+    # Output to console for all others
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Define CACHES variable for DJANGO_HOST production and all other hosts 
+if DJANGO_HOST == "production":
+   # Set cache
+   CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+            'TIMEOUT':'1800',
+            }
+        }
+   CACHE_MIDDLEWARE_SECONDS = 1800
+else: 
+   # No cache for all other hosts
+   pass
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
@@ -184,4 +183,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 
 # STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
